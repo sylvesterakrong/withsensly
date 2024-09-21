453606694c49b0132e2bbc67b1be9320bacae092
Sylvester Akrong <100083798+sylvesterakrong@users.noreply.github.com>
2024-09-21 01:06:53 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Hero from "./components/Hero/page"
 export default function Home() {
   return (
     <main className="px-10 min-h-screen">
-      <h1> page under construction</h1>
+      <h1> page under maintenance</h1>
     </main>
   )
 }
